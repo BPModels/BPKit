c997c6a5046b9eb36f20a605b503e6ed455021ca
sam <916878440@qq.com>
2021-06-11 13:55:43 +0800

    修改BPTabBarControler

---
+++ BPKit/Classes/BPKitConfig.swift
@@ -0,0 +1,31 @@
+//
+//  BPKitConfig.swift
+//  BPKit
+//
+//  Created by samsha on 2021/6/11.
+//
+
+import Foundation
+
+/// 统一配置类
+public class BPKitConfig {
+    public static let share = BPKitConfig()
+    
+    
+
+    // TODD: ==== BPTabBarController ====
+    
+    /// 根视图控制器列表
+    var rootNCList: [UINavigationController] = []
+    
+    /// 添加根视图控制器列表
+    public func addRootNavigationControllers(nvcList: [UINavigationController]) {
+        self.rootNCList = nvcList
+    }
+    
+    /// 未选中时底部Item的文字颜色
+    public var unselectItemColor: UIColor?
+    
+    /// 选中后底部Item的文字颜色
+    public var selectedItemColor: UIColor?
+}
